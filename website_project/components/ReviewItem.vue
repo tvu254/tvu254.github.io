c226447a2dcdb4884a8abd8b6d00fbe335ea612b
tvu254 <tommyvu.t@gmail.com>
2022-03-23 14:17:47 -0500

    fuck you tech

---
+++ website_project/components/ReviewItem.vue
@@ -0,0 +1,78 @@
+<template>
+<div class = "reviewItem" @click="showOptions(review.id)">
+    <div class = "userProfileReview">
+        <div class = "userReviewItem">
+            {{ username }}
+        </div>
+        <div class = "reviewContext"> 
+            Type - {{ review.type }}
+            <div>
+                Genre - {{ review.genre }} 
+            </div>
+        </div>
+        <div class = "reviewItemContent">
+            {{ review.content }}
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+    name: "ReviewItem",
+    setup(props, ctx) {
+
+        const optionsToggle = ref(false);
+
+        function showOptions(id) {
+            ctx.emit('favorite', id)
+            optionsToggle.value = !optionsToggle.value;
+        }
+
+        return {
+            showOptions,
+            optionsToggle
+        }
+    },
+    props:  {
+        username: {
+            type: String,
+            required: true
+        },
+        review: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.reviewItem {
+    padding: 12px;
+    background-color: #ececec;
+    border-radius: 5px;
+    border: 1px solid #0d1424;
+    box-sizing: border-box;
+    cursor: pointer;
+    transition: all 0.24s ease;
+    width: 550px;
+    margin: auto;
+    margin-top: 5px;
+    //font-size: 18px;   --> if I use serif instead of arial
+
+    &:hover {
+        transform: scale(1.1, 1.1);
+    }
+
+    .userReviewItem {
+        font-weight: bold;
+    }
+
+    .reviewContext {
+        font-weight: normal;
+    }
+}
+</style>
---
+++ website_project/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <h1><strong>Welcome to uReview!</strong></h1>
+    <h2> Working on adding genre then saving reviews to the database, the links below don't work anymore because the profile page now loads from dynamodb. These will now have to be fetched.
+    </h2>
+    <div class="userList">
+      <router-link v-for="user in users" :to="{ name: 'UserProfile', params: {userId: user.id} }" :key="user.id">
+        {{ user.username }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { users } from "../assets/users";
+
+export default {
+  name: 'Home',
+  setup() {
+    return {
+      users
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  padding: 50px 5%;
+
+  .userList {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
